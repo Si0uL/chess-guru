9c10a7e4d884c3e03398165aaf8eeddd5f246aa7
Louis Mangin <mangin.louis@gmail.com>
2018-07-02 13:39:43 +0800

    defaulf board in board.py

--- board.py
+++ board.py
@@ -1,6 +1,5 @@
 BOARD = [[{"color": "blank"} for _ in range(8)] for _ in range(8)]
 
-"""
 # Normal start board
 BOARD[7] = [
     {
@@ -73,19 +72,3 @@ BOARD[0] = [
 
 BOARD[6] = [{"color": "white", "type": "pawn"} for _ in range(8)]
 BOARD[1] = [{"color": "black", "type": "pawn"} for _ in range(8)]
-"""
-
-BOARD[0][0] = {'color': 'black', 'type': 'king'}
-BOARD[1][0] = {'color': 'black', 'type': 'pawn'}
-BOARD[1][1] = {'color': 'black', 'type': 'pawn'}
-BOARD[1][2] = {'color': 'black', 'type': 'pawn'}
-BOARD[1][3] = {'color': 'black', 'type': 'pawn'}
-BOARD[6][0] = {'color': 'white', 'type': 'king'}
-BOARD[5][0] = {'color': 'white', 'type': 'pawn'}
-BOARD[6][1] = {'color': 'white', 'type': 'pawn'}
-BOARD[6][2] = {'color': 'white', 'type': 'pawn'}
-BOARD[6][3] = {'color': 'white', 'type': 'pawn'}
-
-BOARD[0][7] = {'color': 'black', 'type': 'rook'}
-BOARD[4][7] = {'color': 'black', 'type': 'rook'}
-BOARD[1][4] = {'color': 'white', 'type': 'knight'}
