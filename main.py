dea274483b4755f9a641d2a9026df7feecc5b0b1
Louis Mangin <louis.mangin@beebryte.com>
2018-12-20 19:41:55 +0100

    Updated the way to launch Flask server

--- main.py
+++ main.py
@@ -106,3 +106,6 @@ def autoplay():
     arow, acol = tree[best_index]['to']
 
     return redirect('/play/{}/{}/{}/{}'.format(srow, scol, arow, acol))
+
+if __name__ == '__main__':
+    app.run(debug=True, threaded=True, host="127.0.0.1", port=5000)
